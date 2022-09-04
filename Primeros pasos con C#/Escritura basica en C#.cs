02a33e8672c972293aa084c130311b669e8019df
Silvia <111834636+silvializarraga@users.noreply.github.com>
2022-09-04 10:41:19 -0700

    Delete Escritura basica en C#.cs

--- Primeros pasos con C#/Escritura basica en C#.cs
+++
@@ -1,9 +0,0 @@
-Console.WriteLine("This is the first line.");
-
-Console.Write("This is ");
-Console.Write("the second ");
-Console.Write("line.");
-
-//Pero se llegaría más fácil al resultado con:
-//Console.WriteLine("This is the first line.");
-//Console.WriteLine("This is the second line.");
